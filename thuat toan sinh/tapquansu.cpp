b39b00a430ea2f29b077098f6d6c41dd3aeaf691
KanCC <dinhcuongnd2001@gmail.com>
2021-08-07 10:27:41 +0700

    update

---
+++ thuat toan sinh/tapquansu.cpp
@@ -0,0 +1,47 @@
+/*
+idea : 
+    sinh to hop 
+    so sanh to hop truoc voi to hop sau
+    so phan tu trung chinh la so thanh vien duoc nghi 
+*/
+#include<bits/stdc++.h>
+
+using namespace std ;
+int n , k , a[100] ;
+set<int> res ;
+
+void init(){
+    res.clear() ;
+    cin>>n>>k;
+    for(int i = 1 ; i <= k ; i++){
+        cin>>a[i] ;
+        res.insert(a[i]) ;
+    }
+}
+
+void solve(){
+    int i = k ;
+    while(a[i] == n - k + i ) i-- ;
+    if(i > 0){
+        a[i] += 1 ; res.insert(a[i]) ;
+        for(int j = i + 1 ; j <= k ; j++){
+            a[j] = a[i] + j - i ;
+            res.insert(a[j]) ;
+        }
+        cout<<res.size() - k;
+    }
+    else{
+        cout<<k ;
+    }
+    cout<<endl;
+}
+
+int main(){
+    int t ;
+    cin>>t ;
+    while(t--){
+        init() ;
+        solve() ;
+    }
+    return 0 ;
+}
